df69309fb0ebb6e9b35638b27ce132d7d59f1133
Han <blacksmith1710@gmail.com>
2025-03-28 21:22:51 +0630

    week-4-start

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
 //Home
-Route::get('/', [HomeController::class, 'index']);
+// Route::get('/', [HomeController::class, 'index']);
+
+Route::resource('post', HomeController::class);
